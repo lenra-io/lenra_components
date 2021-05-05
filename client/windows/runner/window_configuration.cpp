6683824a81f9d94ea5b912119995bd78a99fab57
Thomas <thomas.darocha@lenra.fr>
2021-05-05 13:02:25 +0000

    feature/LENRA-231 webpage data

--- client/windows/runner/window_configuration.cpp
+++ client/windows/runner/window_configuration.cpp
@@ -1,6 +1,6 @@
 #include "window_configuration.h"
 
-const wchar_t* kFlutterWindowTitle = L"lenra_client";
+const wchar_t* kFlutterWindowTitle = L"Lenra";
 const unsigned int kFlutterWindowOriginX = 10;
 const unsigned int kFlutterWindowOriginY = 10;
 const unsigned int kFlutterWindowWidth = 1280;
