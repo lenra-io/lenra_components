e9f9580233820398d0793eb7ca31c159fdb2c079
Emric <emric.pichonnier@lenra.me>
2021-07-29 15:15:44 +0200

    Add test

--- client/test/lenra_components/lenra_toggle_test.dart
+++ client/test/lenra_components/lenra_toggle_test.dart
@@ -1,10 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:fr_lenra_client/lenra_components/layout/lenra_row.dart';
 import 'package:fr_lenra_client/lenra_components/lenra_toggle.dart';
+
+import '../page/lenra_page_test_help.dart';
 
 void main() {
   test('LenraToggle test', () {
     LenraToggle component =
         LenraToggle(value: true, onChanged: (bool _value) {});
     expect(component is LenraToggle, true);
+  });
+
+  testWidgets('LenraToggle with label', (WidgetTester tester) async {
+    await tester.pumpWidget(createComponentTestWidgets(
+      LenraToggle(
+        value: true,
+        onChanged: (bool _value) {},
+        label: "test",
+      ),
+    ));
+
+    expect(
+        (tester.widget(find.byType(LenraRow)) as LenraRow).children.first
+            is Text,
+        true);
+    expect(
+        (tester.widget(find.byType(LenraRow)) as LenraRow).children.elementAt(1)
+            is Switch,
+        true);
   });
 }
