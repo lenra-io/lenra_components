27474d882010639bf2b51f272a5fbf79df73cc2f
jonas-martinez <cbobbxta.fr@gmail.com>
2021-08-17 11:32:34 +0200

    Now handling right and bottom overflow Still need to handle multi-sided overflow

--- lib/component/lenra_dropdown_button.dart
+++ lib/component/lenra_dropdown_button.dart
@@ -109,11 +109,41 @@ class _DropdownState extends State<_Dropdown> {
     WidgetsBinding.instance!.addPostFrameCallback((_) {
       if (overlayOffset == null) {
         var temp = overlayKey.currentContext?.findRenderObject();
+
         print(temp);
         if (temp != null) {
+          var tempOffset = (temp as RenderBox).localToGlobal(Offset.zero);
+          var overlaySize = temp.size;
+          var buttonSize = widget.renderBox.size;
+          var buttonOffset = widget.renderBox.localToGlobal(Offset.zero);
+          var screenSize = MediaQuery.of(context).size;
+
+          bool overflowRight = tempOffset.dx + overlaySize.width > screenSize.width;
+          bool overflowLeft = buttonOffset.dx + buttonSize.width - overlaySize.width < 0;
+          bool overflowBottom = buttonOffset.dy + buttonSize.height + overlaySize.height > screenSize.height;
+          bool overflowTop = buttonOffset.dy - overlaySize.height < 0;
+
+          print(overflowRight);
+          print(overflowLeft);
+          print(overflowBottom);
+          print(overflowTop);
+
           setState(() {
+            var xOffset = 0.0; // Default x Offset, top left of overlay is just under bottom left of button
+            var yOffset = buttonSize.height; // Default y Offset, overlay is just under button
+
             overlayOffset = (temp as RenderBox).localToGlobal(Offset.zero);
-            print(overlayOffset);
+            if (overflowRight && overflowLeft) {}
+
+            if (overflowRight) {
+              xOffset = -(overlaySize.width - buttonSize.width);
+            }
+
+            if (overflowBottom) {
+              yOffset = -overlaySize.height;
+            }
+
+            overlayOffset = Offset(xOffset, yOffset);
           });
         }
       }
@@ -127,9 +157,10 @@ class _DropdownState extends State<_Dropdown> {
     return Stack(
       children: [
         Positioned(
-          top: offset.dy + widget.renderBox.size.height,
+          //top: 1000 + offset.dy + widget.renderBox.size.height,
           child: CompositedTransformFollower(
             offset: overlayOffset ?? Offset(0, widget.renderBox.size.height),
+            //offset: Offset(0, widget.renderBox.size.height),
             link: widget.layerLink,
             child: Material(key: overlayKey, child: widget.menu),
           ),
