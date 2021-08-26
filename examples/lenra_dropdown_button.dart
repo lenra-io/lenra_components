29754c37d0e2c2836c778eef3fcc67ae390a54ed
jonas-martinez <cbobbxta.fr@gmail.com>
2021-08-26 15:31:49 +0200

    Added example

---
+++ examples/lenra_dropdown_button.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:lenra_components/component/lenra_dropdown_button.dart';
+import 'package:lenra_components/lenra_components.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return LenraTheme(
+      themeData: LenraThemeData(),
+      child: MaterialApp(
+        title: 'LenraDropdownButton',
+        home: MyDropdownButton(),
+      ),
+    );
+  }
+}
+
+class MyDropdownButton extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _MyDropdownButtonState();
+}
+
+class _MyDropdownButtonState extends State<MyDropdownButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('LenraMenu'),
+      ),
+      body: LenraColumn(
+        children: [
+          LenraDropdownButton(
+            text: "Dropdown",
+            child: LenraMenu(
+              items: [
+                LenraMenuItem(
+                  text: "MenuItem",
+                  onPressed: () => {},
+                ),
+                LenraMenuItem(
+                  text: "MenuItem2",
+                  onPressed: () => {},
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
