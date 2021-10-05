44582782aae0cb87b9069204ef23d6b415232fd3
Jonas Martinez <36544012+jonas-martinez@users.noreply.github.com>
2021-10-05 11:01:59 +0200

    refactor: Removing useless colorParser (#38)
    * Removed color parser

--- lib/utils/color_parser.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/painting.dart';
-
-extension ColorParser on String {
-  Color parseColor() {
-    String hex = replaceFirst('#', '');
-    int hexb16 = int.parse('FF$hex', radix: 16);
-    return Color(hexb16);
-  }
-}
