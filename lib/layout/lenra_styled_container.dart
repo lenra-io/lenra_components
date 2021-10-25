49ed107ecefce24f69c46a8d4c3370adbcbcffab
jonas-martinez <cbobbxta.fr@gmail.com>
2021-10-25 14:53:34 +0200

    feat: Preparing container rework

--- lib/layout/lenra_styled_container.dart
+++ lib/layout/lenra_styled_container.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/widgets.dart';
+import 'package:lenra_components/theme/lenra_theme.dart';
 
 class LenraStyledContainer extends StatelessWidget {
   final Widget child;
@@ -7,6 +8,7 @@ class LenraStyledContainer extends StatelessWidget {
   final BoxShadow? boxShadow;
   final Color? color;
   final EdgeInsets? padding;
+  final BoxConstraints? constraints;
 
   const LenraStyledContainer({
     Key? key,
@@ -16,19 +18,23 @@ class LenraStyledContainer extends StatelessWidget {
     this.boxShadow,
     this.color,
     this.padding,
+    this.constraints,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    var theme = LenraTheme.of(context);
+
     return Container(
       child: child,
-      padding: padding,
+      padding: padding != null ? padding! * theme.baseSize : padding,
       decoration: BoxDecoration(
         color: color,
         border: border,
         borderRadius: borderRadius,
         boxShadow: boxShadow != null ? [boxShadow!] : null,
       ),
+      constraints: constraints,
     );
   }
 }
