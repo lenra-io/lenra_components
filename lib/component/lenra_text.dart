cb28d361555a50194e2e9c6fc0a951daef8d3d9e
ClementGld <clement.guillaud@lenra.io>
2021-11-05 14:24:31 +0100

    feat: rework text component

--- lib/component/lenra_text.dart
+++ lib/component/lenra_text.dart
@@ -3,15 +3,15 @@ import 'package:flutter/widgets.dart';
 class LenraText extends StatelessWidget {
   final String text;
   final TextStyle? style;
-  final List<TextSpan> children;
-  final Locale locale;
+  final List<TextSpan>? children;
+  final Locale? locale;
   final String? semanticsLabel;
   final bool? spellOut;
 
-  const LenraText(
+  const LenraText({
     this.children,
     this.locale,
-    this.text, {
+    required this.text,
     this.style,
     this.semanticsLabel,
     this.spellOut,
