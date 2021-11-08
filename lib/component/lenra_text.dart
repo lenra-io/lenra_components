aa3c90f532eed9bb05cbb790dd41a6fe3cb6fa9a
jonas-martinez <cbobbxta.fr@gmail.com>
2021-11-08 11:49:42 +0100

    Refactor

--- lib/component/lenra_text.dart
+++ lib/component/lenra_text.dart
@@ -2,30 +2,32 @@ import 'package:flutter/widgets.dart';
 
 class LenraText extends StatelessWidget {
   final String text;
-  final TextStyle? style;
   final List<TextSpan>? children;
+  final TextStyle? style;
   final Locale? locale;
   final String? semanticsLabel;
   final bool? spellOut;
 
   const LenraText({
-    this.children,
-    this.locale,
+    Key? key,
     required this.text,
+    this.children,
     this.style,
+    this.locale,
     this.semanticsLabel,
     this.spellOut,
-    Key? key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Text.rich(TextSpan(
-        children: children,
-        text: text,
-        locale: locale,
-        style: style,
-        semanticsLabel: semanticsLabel,
-        spellOut: spellOut));
+    return Text.rich(
+      TextSpan(
+          text: text,
+          children: children,
+          style: style,
+          locale: locale,
+          semanticsLabel: semanticsLabel,
+          spellOut: spellOut),
+    );
   }
 }
