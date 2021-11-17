7806fdf3d0c5d744e600203bead4de5d21756ca6
jonas-martinez <cbobbxta.fr@gmail.com>
2021-11-17 19:17:45 +0100

    Added more tests

--- test/component/lenra_checkbox_test.dart
+++ test/component/lenra_checkbox_test.dart
@@ -1,6 +1,7 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:lenra_components/component/lenra_checkbox.dart';
+import 'package:lenra_components/theme/lenra_checkbox_style.dart';
 
 import '../utils/lenra_page_test_help.dart';
 
@@ -60,5 +61,57 @@ void main() {
     await tester.tap(find.byType(LenraCheckbox));
     await tester.pumpAndSettle();
     expect(value, false);
+  });
+
+  testWidgets('Test LenraCheckbox disabled', (WidgetTester tester) async {
+    await tester.pumpWidget(createComponentTestWidgets(
+      const LenraCheckbox(
+        value: true,
+        onPressed: null,
+      ),
+    ));
+
+    /// According to flutter's documentation, when the `onChanged` property is `null` then the `Checkbox` is disabled.
+    /// https://api.flutter.dev/flutter/material/Checkbox/onChanged.html
+    expect(tester.widget<Checkbox>(find.byType(Checkbox)).onChanged, null);
+  });
+
+  testWidgets('Test LenraCheckbox style', (WidgetTester tester) async {
+    bool value = false;
+
+    await tester.pumpWidget(createComponentTestWidgets(
+      StatefulBuilder(
+        builder: (BuildContext context, StateSetter setState) {
+          return LenraCheckbox(
+            value: value,
+            onPressed: (bool? v) {
+              setState(() {
+                value = v!;
+              });
+            },
+            style: LenraCheckboxStyle(
+              activeColor: Colors.blue.shade50,
+              checkColor: Colors.blue.shade100,
+              focusColor: Colors.blue.shade200,
+              hoverColor: Colors.blue.shade300,
+              splashRadius: 10.0,
+              visualDensity: VisualDensity.compact,
+              shape: const CircleBorder(),
+              side: const BorderSide(width: 10),
+            ),
+          );
+        },
+      ),
+    ));
+
+    Checkbox checkbox = tester.widget<Checkbox>(find.byType(Checkbox));
+    expect(checkbox.activeColor, Colors.blue.shade50);
+    expect(checkbox.checkColor, Colors.blue.shade100);
+    expect(checkbox.focusColor, Colors.blue.shade200);
+    expect(checkbox.hoverColor, Colors.blue.shade300);
+    expect(checkbox.splashRadius, 10.0);
+    expect(checkbox.visualDensity, VisualDensity.compact);
+    expect(checkbox.shape.runtimeType, CircleBorder);
+    expect(checkbox.side!.width, 10);
   });
 }
