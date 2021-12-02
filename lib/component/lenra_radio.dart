34440b0aad3d55305b371192996f35e283bf6b94
jonas-martinez <cbobbxta.fr@gmail.com>
2021-12-02 14:22:22 +0100

    Added comments

--- lib/component/lenra_radio.dart
+++ lib/component/lenra_radio.dart
@@ -2,30 +2,44 @@ import 'package:flutter/material.dart';
 import 'package:lenra_components/theme/lenra_radio_style.dart';
 
 class LenraRadio<T> extends StatelessWidget {
+  /// The value of the LenraRadio.
+  final T value;
+
+  /// The selected value for a group of LenraRadio.
+  final T? groupValue;
+
+  /// The callback when the LenraRadio is clicked.
+  final ValueChanged<T?>? onPressed;
+
+  /// The style of the LenraRadio.
   final LenraRadioStyle? style;
+
+  /// Whether this LenraRadio is focused initially.
   final bool autofocus;
   final FocusNode? focusNode;
-  final T? groupValue;
   final MaterialTapTargetSize? materialTapTargetSize;
-  final ValueChanged<T?>? onPressed;
+
+  /// Whether the LenraRadio can be unselected after selecting it.
   final bool toggleable;
-  final T value;
 
   const LenraRadio({
     Key? key,
+    required this.value,
+    required this.groupValue,
+    required this.onPressed,
     this.style,
     this.autofocus = false,
     this.focusNode,
-    required this.groupValue,
     this.materialTapTargetSize,
-    required this.onPressed,
     this.toggleable = false,
-    required this.value,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Radio<T>(
+      value: value,
+      groupValue: groupValue,
+      onChanged: onPressed,
       activeColor: style?.activeColor,
       focusColor: style?.focusColor,
       hoverColor: style?.hoverColor,
@@ -34,10 +48,7 @@ class LenraRadio<T> extends StatelessWidget {
       autofocus: autofocus,
       focusNode: focusNode,
       materialTapTargetSize: materialTapTargetSize,
-      groupValue: groupValue,
-      onChanged: onPressed,
       toggleable: toggleable,
-      value: value,
     );
   }
 }
