0c02918f311d7fee2aeffdfde0d1e3c41b0a5bd0
jonas-martinez <cbobbxta.fr@gmail.com>
2021-12-02 15:13:37 +0100

    More examples

--- examples/flutter_playground/lib/pages/slider_example.dart
+++ examples/flutter_playground/lib/pages/slider_example.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lenra_components/component/lenra_slider.dart';
+import 'package:lenra_components/component/lenra_text.dart';
 import 'package:lenra_components/layout/lenra_flex.dart';
 import 'package:lenra_components/lenra_components.dart';
 
@@ -14,22 +15,37 @@ class SliderExample extends StatefulWidget {
 
 class _SliderExampleState extends State<SliderExample> {
   var value = 0.5;
+  String label = "Hello World!";
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: LenraFlex(
-        direction: Axis.horizontal,
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        direction: Axis.vertical,
         children: [
           LenraSlider(
-            divisions: 100,
+            divisions: 4,
             value: value,
             onChanged: (v) {
               setState(() {
                 value = v;
+                label = v.toString();
+              });
+            },
+            onChangeStart: (v) {
+              setState(() {
+                label = "Start $v";
+              });
+            },
+            onChangeEnd: (v) {
+              setState(() {
+                label = "End $v";
               });
             },
           ),
+          LenraText(text: label),
         ],
       ),
     );
