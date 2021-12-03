5978172992cc74d600a88ac7fc4f77ee1fb5a106
jonas-martinez <cbobbxta.fr@gmail.com>
2021-12-03 14:30:11 +0100

    Reworked tests

--- test/component/lenra_toggle_test.dart
+++ test/component/lenra_toggle_test.dart
@@ -4,59 +4,55 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:lenra_components/component/lenra_toggle.dart';
 import 'package:lenra_components/theme/lenra_toggle_syle.dart';
 
+import '../utils/lenra_page_test_help.dart';
+
 void main() {
-  testWidgets('Test Basic LenraToggle', (WidgetTester tester) async {
-    var _giveVerse = true;
-
-    LenraToggleStyle style = const LenraToggleStyle(
-      activeTrackColor: Colors.black,
-      focusColor: Colors.amber,
-      inactiveThumbColor: Colors.amber,
-      hoverColor: Colors.amber,
-      materialTapTargetSize: MaterialTapTargetSize.padded,
-    );
-
-    LenraToggle lenraToggle = LenraToggle(
-      value: _giveVerse,
-      onPressed: null,
-      style: style,
-      autofocus: true,
-      splashRadius: 2,
-      dragStartBehavior: DragStartBehavior.down,
-    );
-
-    expect(lenraToggle.value, true);
-    expect(style.activeTrackColor, Colors.black);
-    expect(style.focusColor, Colors.amber);
-    expect(lenraToggle.autofocus, true);
-    expect(lenraToggle.splashRadius, 2);
-    expect(lenraToggle.dragStartBehavior, DragStartBehavior.down);
-    expect(style.inactiveThumbColor, Colors.amber);
-    expect(style.hoverColor, Colors.amber);
-    expect(style.materialTapTargetSize, MaterialTapTargetSize.padded);
-    expect(lenraToggle.onPressed, null);
-  });
-
-  testWidgets('Test onPressed active', (WidgetTester tester) async {
+  testWidgets('LenraToggle should call onPressed when clicked.', (WidgetTester tester) async {
     var value = false;
-    await tester.pumpWidget(Directionality(
-        textDirection: TextDirection.ltr,
-        child: StatefulBuilder(builder: (BuildContext context, StateSetter setState) {
-          return Material(
-              child: LenraToggle(
-            key: const Key('myKey'),
+
+    await tester.pumpWidget(
+      createComponentTestWidgets(
+        StatefulBuilder(builder: (BuildContext context, StateSetter setState) {
+          return LenraToggle(
+            key: const Key('toggle'),
             value: value,
             onPressed: (newValue) {
               setState(() {
                 value = newValue;
               });
             },
-          ));
-        })));
+          );
+        }),
+      ),
+    );
 
     expect(value, false);
-    await tester.tap(find.byKey(const Key('myKey')));
+    await tester.tap(find.byKey(const Key('toggle')));
     await tester.pump();
     expect(value, true);
+  });
+
+  testWidgets('LenraToggle style is properly passed to Flutter\'s Toggle.', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      createComponentTestWidgets(
+        LenraToggle(
+          key: const Key('toggle'),
+          value: true,
+          onPressed: (v) {},
+          style: const LenraToggleStyle(
+            inactiveThumbColor: Colors.red,
+            activeTrackColor: Colors.red,
+            hoverColor: Colors.red,
+            focusColor: Colors.red,
+          ),
+        ),
+      ),
+    );
+
+    Switch toggle = tester.widget(find.byType(Switch));
+    expect(toggle.inactiveThumbColor, Colors.red);
+    expect(toggle.activeTrackColor, Colors.red);
+    expect(toggle.hoverColor, Colors.red);
+    expect(toggle.focusColor, Colors.red);
   });
 }
