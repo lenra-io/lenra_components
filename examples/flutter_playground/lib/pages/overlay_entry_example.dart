547fc96923eebc7b16dc5c970ee7c7c0a8787a0e
jonas-martinez <cbobbxta.fr@gmail.com>
2022-03-21 17:08:52 +0100

    Better example

--- examples/flutter_playground/lib/pages/overlay_entry_example.dart
+++ examples/flutter_playground/lib/pages/overlay_entry_example.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lenra_components/component/lenra_text.dart';
 import 'package:lenra_components/layout/lenra_overlay_entry.dart';
 import 'package:lenra_components/lenra_components.dart';
 
@@ -14,19 +15,19 @@ class OverlayEntryExample extends StatefulWidget {
 class _OverlayEntryExampleState extends State<OverlayEntryExample> {
   Widget test = const SizedBox.shrink();
   bool showOverlay = false;
-  bool showOverlay2 = true;
   bool showOverlay3 = true;
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: LenraFlex(
-        direction: Axis.horizontal,
+        direction: Axis.vertical,
+        spacing: 2,
         children: [
+          LenraText(text: "You can show another overlay by clicking on the button below."),
           LenraButton(
-            text: "Show Overlay",
+            text: "Show",
             onPressed: () {
-              print("Show Overlay");
               setState(() {
                 showOverlay = true;
               });
@@ -34,19 +35,33 @@ class _OverlayEntryExampleState extends State<OverlayEntryExample> {
           ),
           LenraOverlayEntry(
             showOverlay: showOverlay,
-            child: LenraFlex(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  width: 100,
-                  height: 100,
-                  child: LenraButton(
-                    text: "Foo",
-                    onPressed: () {},
+            child: SizedBox(
+              width: double.infinity,
+              height: double.infinity,
+              child: Container(
+                child: Center(
+                  child: Container(
+                    color: LenraColorThemeData.greyNature,
+                    child: LenraFlex(
+                      direction: Axis.vertical,
+                      spacing: 2,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        LenraText(text: "This is an overlay entry"),
+                        LenraButton(
+                          text: "Close",
+                          onPressed: () {
+                            setState(() {
+                              showOverlay = false;
+                            });
+                          },
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ],
+              ),
             ),
           ),
           LenraOverlayEntry(
@@ -55,15 +70,26 @@ class _OverlayEntryExampleState extends State<OverlayEntryExample> {
               width: double.infinity,
               height: double.infinity,
               child: Container(
-                color: Colors.red,
+                color: LenraColorThemeData.greyLight,
                 child: Center(
-                  child: LenraButton(
-                      text: "Foo3",
-                      onPressed: () {
-                        setState(() {
-                          showOverlay3 = false;
-                        });
-                      }),
+                  child: LenraFlex(
+                    spacing: 2,
+                    direction: Axis.vertical,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      LenraText(text: "This is an overlay."),
+                      LenraText(text: "You can close it by clicking on the button."),
+                      LenraButton(
+                        text: "Close",
+                        onPressed: () {
+                          setState(() {
+                            showOverlay3 = false;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
