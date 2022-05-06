485a8eae4bec4fbf77db392e11a82e21b367cad2
Jonas Martinez <36544012+jonas-martinez@users.noreply.github.com>
2022-05-06 15:14:34 +0200

    fix: Fix overlayEntry crash (#98)

--- lib/layout/lenra_overlay_entry.dart
+++ lib/layout/lenra_overlay_entry.dart
@@ -34,7 +34,7 @@ class _LenraOverlayEntryState extends State<LenraOverlayEntry> {
   @override
   void deactivate() {
     super.deactivate();
-    if (overlayEntry.mounted) {
+    if (widget.showOverlay && overlayEntry.mounted) {
       removeOverlay();
     }
   }
