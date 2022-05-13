1669fab18c02711fda0efef79a965f0c93adb2eb
ClementGld <clement.guillaud@lenra.io>
2022-05-13 14:28:57 +0200

    fix: errors must be in english

--- lib/utils/form_validators.dart
+++ lib/utils/form_validators.dart
@@ -13,7 +13,7 @@ Validator validator(List<Validator> validators) {
 Validator checkLength({int min = 0, int max = 0, String? error}) {
   return (String? value) {
     if (value == null || value.length > max || value.length < min) {
-      return error ?? 'Doit contenir entre $min et $max caractères';
+      return error ?? 'Must contain between $min and $max characters';
     }
     return null;
   };
@@ -22,7 +22,7 @@ Validator checkLength({int min = 0, int max = 0, String? error}) {
 Validator checkNotEmpty({String? error}) {
   return (String? value) {
     if (value == null || value.isEmpty) {
-      return error ?? 'Le champ ne doit pas être vide';
+      return error ?? 'The field should not be empty';
     }
     return null;
   };
@@ -31,7 +31,7 @@ Validator checkNotEmpty({String? error}) {
 Validator checkEmailFormat({String? error}) {
   return (String? value) {
     if (value == null || !RegExp(r"[^@]+@[^\.]+\..+").hasMatch(value)) {
-      return error ?? "Email invalide";
+      return error ?? "Invalid email";
     }
     return null;
   };
@@ -41,7 +41,7 @@ Validator checkGitRepoFormat({String? error}) {
   return (String? value) {
     if (value == null ||
         !RegExp(r"((git|http(s)?)|(git@[\w\.]+))(:(//)?)([\w\.@\:/\-~]+)(\.git)(/)?").hasMatch(value)) {
-      return error ?? "Dépot git invalide";
+      return error ?? "Invalid git repository";
     }
     return null;
   };
@@ -50,7 +50,7 @@ Validator checkGitRepoFormat({String? error}) {
 Validator checkPassword({String? error}) {
   return (String? value) {
     if (value == null || !RegExp(r"(?=.*[a-z])(?=.*[A-Z])(?=.*\W)").hasMatch(value)) {
-      return error ?? "1 majuscule, 1 minuscule et 1 caractère spécial.";
+      return error ?? "1 uppercase, 1 lowercase and 1 special character.";
     }
     return null;
   };
